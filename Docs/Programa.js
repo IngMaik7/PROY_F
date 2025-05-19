3807cf7ea04b702e6aed7c8096c0d5a4e45f65b3
Michael Ramirez <michaelrp2000@gmail.com>
2025-05-19 12:57:02 -0500

    V.10

--- Docs/Programa.js
+++ Docs/Programa.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([4.611755, -74.083450], 13);
+var map = L.map('map').setView([4.615326, -74.088646], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([4.615326, -74.088646]).addTo(map);
 
