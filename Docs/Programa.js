f26368937f08989994309d55da5b7b1aa72d620d
Michael Ramirez <michaelrp2000@gmail.com>
2025-05-21 15:56:11 -0500

    V.08

--- Docs/Programa.js
+++ Docs/Programa.js
@@ -7,3 +7,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([4.611998, -74.083205]).addTo(map);
 
+var circle = L.circle([4.611998, -74.083205], {
+    color: 'green',
+    fillColor: '#f02',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+
+
